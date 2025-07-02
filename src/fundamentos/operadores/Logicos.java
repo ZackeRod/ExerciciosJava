d932604d5ee7949e032b8a5371dbc9c56c04ab6f
Zacker <zackerod@gmail.com>
2025-07-02 17:33:09 -0300

    Adicionado Commit's

--- src/fundamentos/operadores/Logicos.java
+++ src/fundamentos/operadores/Logicos.java
@@ -4,42 +4,42 @@ public class Logicos {
     public static void main(String[] args) {
 
         boolean condicao1 = true;
-        boolean condicao2 = 3 > 7;
+        boolean condicao2 = 3 > 7; // FALSE
 
-        System.out.println(condicao1 && condicao2);
-        System.out.println(condicao1 && !condicao2);
-        System.out.println(condicao1 || condicao2);
-        System.out.println(condicao1 ^ condicao2);
-        System.out.println(!!condicao1);
-        System.out.println(!condicao1);
-        System.out.println(!condicao2);
+        System.out.println(condicao1 && condicao2);   // F
+        System.out.println(condicao1 && !condicao2);  // V
+        System.out.println(condicao1 || condicao2);   // V
+        System.out.println(condicao1 ^ condicao2);    // V
+        System.out.println(!!condicao1);              // V                // CONVERTEU 2 VEZES
+        System.out.println(!condicao1);               // F
+        System.out.println(!condicao2);               // V
 
         // Tabela verdade E (AND)
         System.out.println("\nTabela verdade E (AND)");
-        System.out.println(true && true);
-        System.out.println(true && false);
-        System.out.println(false && true);
-        System.out.println(false && false);
+        System.out.println(true && true);   // V
+        System.out.println(true && false);  // F
+        System.out.println(false && true);  // F
+        System.out.println(false && false); // F
 
 
         // Tabela verdade OU (OR)
         System.out.println("\nTabela verdade OU (OR)");
-        System.out.println(true || true);
-        System.out.println(true || false);
-        System.out.println(false || true);
-        System.out.println(false || false);
+        System.out.println(true || true);   // V
+        System.out.println(true || false);  // V
+        System.out.println(false || true);  // V
+        System.out.println(false || false); // F
 
         // Tabela verdade OU Exclusivo (XOR)
         System.out.println("\nTabela verdade OU Exclusivo (XOR)");
-        System.out.println(true ^ true);
-        System.out.println(true ^ false);
-        System.out.println(false ^ true);
-        System.out.println(false ^ false);
+        System.out.println(true ^ true);   // F
+        System.out.println(true ^ false);  // V
+        System.out.println(false ^ true);  // V
+        System.out.println(false ^ false); // F
 
         // Tabela verdade NOT
         System.out.println("\nTabela verdade NOT");
-        System.out.println(!true);
-        System.out.println(!false);
+        System.out.println(!true);  // F
+        System.out.println(!false); // V
 
 
     }
